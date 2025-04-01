f298486522e4f98423ec8a7b2a720f009ce802fe
teodord25 <djuric.teodor25@gmail.com>
2025-04-01 07:44:19 +0200

    add flake, change test name

---
+++ flake.nix
@@ -0,0 +1,54 @@
+{
+  description = "Dev shell for Velari (Minecraft mod with Java + Rust + JDTLS)";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+      in {
+        devShell = pkgs.mkShell {
+          name = "velari-dev-shell";
+
+          buildInputs = [
+            # Java dev env
+            pkgs.openjdk21
+            pkgs.gradle
+            pkgs.jdt-language-server
+
+            # Rust toolchain
+            pkgs.rustc
+            pkgs.cargo
+            pkgs.clippy
+            pkgs.rustfmt
+
+            # Native dependencies for GLFW / AWT / OpenGL
+            pkgs.glfw
+            pkgs.mesa
+            pkgs.libGL
+            pkgs.xorg.libX11
+            pkgs.xorg.libXext
+            pkgs.xorg.libXrandr
+            pkgs.xorg.libXcursor
+            pkgs.xorg.libXi
+            pkgs.xorg.libXxf86vm
+            pkgs.xorg.libXinerama
+            pkgs.xorg.libXcomposite
+            pkgs.xorg.libXdamage
+            pkgs.xorg.libXfixes
+            pkgs.xorg.libXtst
+            pkgs.xorg.libxcb
+          ];
+
+          shellHook = ''
+            echo "🪐 Welcome to Velari dev shell"
+            echo "Java, Rust, Gradle, and JDTLS are ready."
+            echo "Use './gradlew runClient' to test the mod."
+          '';
+        };
+      });
+}
