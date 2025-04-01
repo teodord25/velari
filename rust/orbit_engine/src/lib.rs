4be918f0e1387841bc6270e1a40ba5672329c684
teodord25 <djuric.teodor25@gmail.com>
2025-04-01 08:30:15 +0200

    add basic FFI Hello world

---
+++ rust/orbit_engine/src/lib.rs
@@ -0,0 +1,15 @@
+use jni::JNIEnv;
+use jni::objects::JClass;
+use jni::sys::jstring;
+
+/// This is the function Java will call
+#[unsafe(no_mangle)]
+pub extern "system" fn Java_com_velari_HelloNative_getMessage(
+    env: JNIEnv,
+    _class: JClass,
+) -> jstring {
+    let output = env
+        .new_string("Hello from Rust!")
+        .expect("Couldn't create Java string!");
+    output.into_raw()
+}
