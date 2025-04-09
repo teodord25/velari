9215769a871fc7753af2cd193fcba07080109af6
teodord25 <djuric.teodor25@gmail.com>
2025-04-09 11:48:43 +0200

    add second block with different registration method

--- src/main/java/com/github/teodord25/velari/Velari.java
+++ src/main/java/com/github/teodord25/velari/Velari.java
@@ -1,6 +1,7 @@
 package com.github.teodord25.velari;
 
 import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.block.SlabBlock;
 import net.minecraft.world.level.block.state.BlockBehaviour;
 import net.neoforged.bus.api.IEventBus;
 import net.neoforged.fml.common.Mod;
@@ -13,8 +14,17 @@ public class Velari {
 
     public static final DeferredRegister.Blocks BLOCKS = DeferredRegister.createBlocks(Velari.MODID);
 
-    public static final DeferredBlock<Block> EXAMPLE_BLOCK = BLOCKS.registerSimpleBlock(
+    // NOTE: used for simple blocks that use Block::new
+    public static final DeferredBlock<Block> joe_block = BLOCKS.registerSimpleBlock(
         "joe_block",
+        BlockBehaviour.Properties
+            .of()
+            .lightLevel((BlockState) -> 1));
+
+    // NOTE: if you want to pass a subclass of block use this
+    public static final DeferredBlock<Block> tom_trtan_block = BLOCKS.registerBlock(
+        "tom_trtan",
+        SlabBlock::new,
         BlockBehaviour.Properties
             .of()
             .lightLevel((BlockState) -> 15));
