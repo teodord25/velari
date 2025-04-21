fb7c1798701d1c9d7d64f78d78ebf1e38aa8368a
teodord25 <djuric.teodor25@gmail.com>
2025-04-21 20:27:27 +0200

    fix modid

--- src/main/java/com/github/teodord25/velari/client/VelariClient.java
+++ src/main/java/com/github/teodord25/velari/client/VelariClient.java
@@ -1,12 +1,14 @@
 package com.github.teodord25.velari.client;
 
+import com.github.teodord25.velari.Velari;
+
 import net.minecraft.client.Minecraft;
 import net.neoforged.api.distmarker.Dist;
 import net.neoforged.bus.api.IEventBus;
 import net.neoforged.fml.common.Mod;
 
 /// TODO: client only code goes here?
-@Mod(value = "examplemod", dist = Dist.CLIENT) 
+@Mod(value = Velari.MODID, dist = Dist.CLIENT) 
 public class VelariClient {
     public VelariClient(IEventBus modBus) {
         // Perform logic in that should only be executed on the physical client
--- src/main/java/com/github/teodord25/velari/server/VelariServer.java
+++ src/main/java/com/github/teodord25/velari/server/VelariServer.java
@@ -1,12 +1,14 @@
 package com.github.teodord25.velari.server;
 
+import com.github.teodord25.velari.Velari;
+
 import net.neoforged.api.distmarker.Dist;
 import net.neoforged.bus.api.IEventBus;
 import net.neoforged.fml.common.Mod;
 
 /// TODO: client only code goes here?
 
-@Mod(value = "examplemod", dist = Dist.DEDICATED_SERVER) 
+@Mod(value = Velari.MODID, dist = Dist.DEDICATED_SERVER) 
 public class VelariServer {
     public VelariServer(IEventBus modBus) {
         // Perform logic in that should only be executed on the physical server
