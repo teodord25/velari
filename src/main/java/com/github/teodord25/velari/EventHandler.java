bc15cb03aedec17437f3be3a8bc203468428c261
teodord25 <djuric.teodor25@gmail.com>
2025-04-23 13:54:04 +0200

    add priority to hanlder

--- src/main/java/com/github/teodord25/velari/EventHandler.java
+++ src/main/java/com/github/teodord25/velari/EventHandler.java
@@ -2,13 +2,15 @@ package com.github.teodord25.velari;
 
 import net.minecraft.network.chat.Component;
 import net.minecraft.server.level.ServerPlayer;
+import net.neoforged.bus.api.EventPriority;
 import net.neoforged.bus.api.SubscribeEvent;
 import net.neoforged.fml.common.EventBusSubscriber;
 import net.neoforged.neoforge.event.entity.living.LivingEvent;
 
 @EventBusSubscriber(modid = Velari.MODID)
 public class EventHandler {
-    @SubscribeEvent
+    // NOTE: handlers can one of 5 priority settings, they will be executed from highest to lowest
+    @SubscribeEvent(priority = EventPriority.HIGH)
     public static void test(LivingEvent.LivingJumpEvent event) {
         var entity = event.getEntity();
         if (entity instanceof ServerPlayer player) {
