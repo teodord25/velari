ca522570f8cbc114178bc884960d38afca94407b
teodord25 <djuric.teodor25@gmail.com>
2025-05-04 01:54:39 +0200

    downgrade Config.java to 1.21.1

--- src/main/java/com/github/teodord25/velari/Config.java
+++ src/main/java/com/github/teodord25/velari/Config.java
@@ -56,7 +56,7 @@ public class Config
 
         // convert the list of strings into a set of items
         items = ITEM_STRINGS.get().stream()
-                .map(itemName -> BuiltInRegistries.ITEM.getValue(ResourceLocation.parse(itemName)))
+                .map(itemName -> BuiltInRegistries.ITEM.get(ResourceLocation.parse(itemName)))
                 .collect(Collectors.toSet());
     }
 }
